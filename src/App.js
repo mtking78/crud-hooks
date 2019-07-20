719083c2d22a14c24d2a500b05a211dc7576d23d
mtking78 <mtking78@gmail.com>
2019-07-19 23:15:07 -0700

    Created UpdateUserForm component, function, and currentUser state.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { useState }from 'react';
 import './App.css';
 import UserTable from './tables/UserTable';
 import AddUserForm from './forms/AddUserForm';
+import UpdateUserForm from './forms/UpdateUserForm';
 
 const App = () => {
 
-  // Sample data
+  // ***** Sample data *****
   const usersData = [
     { id: 1, name: 'Alfred', username: 'alBot3000' },
     { id: 2, name: 'Barry', username: 'bozo123' },
@@ -13,13 +14,38 @@ const App = () => {
     { id: 4, name: 'Doug', username: 'turboDoug' }
   ]
 
+  const initialFormState = { id: null, name: '', username: '' }
+
+  // ***** Setting the state *****
   const [users, setUsers] = useState(usersData)
+  // Make a state for edit mode, begin as false.
+  const [editing, setEditing] =useState(false)
+  // Take id of chosen user and apply to currentUser state.
+  const [currentUser, setCurrentUser] = useState(initialFormState)
+
+  // ***** CRUD operations *****
 
   // Take the 'user' object as a parameter and add onto the users array of objects.
   // '...users' ensures all the previous users remain in the array.
   const addUser = user => {
     user.id = users.length + 1
     setUsers([...users, user])
+  }
+
+  // Map through the users to find matching id.
+  const updateUser = (id, updatedUser) => {
+    setEditing(false)
+    setUsers(users.map(user => (user.id === id ? updatedUser : user)))
+  }
+
+  // Turn on editing mode, set currentUser so it can be used in UpdateUserForm props.
+  const editRow = user => {
+    setEditing(true)
+    setCurrentUser({
+      id: user.id,
+      name: user.name,
+      username: user.username
+    })
   }
 
   const deleteUser = id => {
@@ -31,12 +57,32 @@ const App = () => {
       <h1>CRUD App with Hooks</h1>
       <div className="flex-row">
         <div className="flex-large">
-          <h2>Add User</h2>
-          <AddUserForm addUser={ addUser } />
+          {/* Ternary operator to either show the add or edit user div. */}
+          {editing ? (
+            <div>
+              <h2>Update User</h2>
+              <UpdateUserForm
+                // Pass all functions to the editing component.
+                editing={editing}
+                setEditing={setEditing}
+                currentUser={currentUser}
+                updateUser={updateUser}
+              />
+            </div>
+          ) : (
+            <div>
+              <h2>Add User</h2>
+              <AddUserForm addUser={ addUser } />
+            </div>
+          )}
         </div>
         <div className="flex-large">
           <h2>View Users</h2>
-          <UserTable users={ users } deleteUser={ deleteUser }/>
+          <UserTable
+            users={ users }
+            editRow={ editRow }
+            deleteUser={ deleteUser }
+          />
         </div>
       </div>
     </div>
---
+++ src/forms/UpdateUserForm.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+
+const UpdateUserForm = props => {
+  const [user, setUser] = useState(props.currentUser)
+
+  const handleInputChange = event => {
+    const { name, value } = event.target
+    setUser({ ...user, [name]: value })
+  }
+
+  return (
+    <form
+      onSubmit={event => {
+        event.preventDefault()
+
+        props.updateUser(user.id, user)
+      }}
+    >
+      <label>Name</label>
+      <input
+        type="text"
+        name="name"
+        value={ user.name }
+        onChange={ handleInputChange }
+      />
+      <label>UserName</label>
+      <input
+        type="text"
+        name="username"
+        value={ user.username }
+        onChange={ handleInputChange }
+      />
+      <button>Update User</button>
+      {/* Set editing back to false, clear the form */}
+      <button
+        className="button muted-button"
+        onClick={() => props.setEditing(false)}
+      >
+        Cancel
+      </button>
+    </form>
+  )
+}
+
+export default UpdateUserForm
